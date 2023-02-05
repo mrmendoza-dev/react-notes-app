595203e406360e32ba4c6dbe168b434397e46cc7
Steven Mendoza <smendoza171@gmail.com>
2023-02-05 07:20:08 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
+import Notes from "./components/Notes/Notes";
+
+function App() {
+  return (
+    <div className="App">
+      <Notes />
+    </div>
+  );
+}
+
+export default App;
